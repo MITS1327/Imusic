612d5e5ced2519d54582df779bb9a22cb73d8f55
MITS <mishacsgo2020@mail.ru>
2022-05-14 15:04:40 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
+const axios = require('axios');
+
 // initialization variables
+
 const searchBtn = document.querySelector('.nav__li_btn-search');
 const searchInput = document.querySelector('.search__input');
 const burger = document.querySelector('.burger');
@@ -9,6 +12,7 @@ const profileAvatar = document.querySelector('.profile__avatar');
 
 
 // open search input
+
 searchBtn.addEventListener('click', () => {
 
   searchInput.classList.toggle('input-active');
@@ -17,6 +21,7 @@ searchBtn.addEventListener('click', () => {
 
 
 // open burger menu
+
 burger.addEventListener('click', () => {
 
   profile.classList.toggle('menu-opened');
@@ -27,8 +32,46 @@ burger.addEventListener('click', () => {
 
 
 // player control
+
 playBtn.addEventListener('click', () => {
 
   playBtn.classList.toggle('stop');
 
 });
+
+// get token
+
+async function getData () {
+
+  axios({
+    method: 'post',
+    url: 'https://api.im.am/test/register',
+    data: {
+      "uuid": "465a4fafa45sfa46"
+    },
+    headers : {
+      "Content-Type" : "multipart/form-data",
+      "Accept" : "*/*",
+    }
+  });
+
+
+
+
+
+
+
+
+  // const req = await fetch('https://api.im.am/test/register', {
+  //   method: 'POST',
+  //   headers: {
+      // "Content-Type" : "multipart/form-data",
+      // "Accept" : "*/*",
+  //   },
+  //   body: JSON.stringify({
+  //     "uuid": "465a4fafa45sfa46"
+  //   }),
+  // });
+}
+
+await getData();
